5b488bd00a843c29ae691009b0bd943303e659c9
koby-git <54393994+koby-git@users.noreply.github.com>
2021-03-02 00:59:06 +0200

    prod

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
-  });
+  // app.use(express.static('client/build'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  // });
 }
 const PORT = process.env.PORT || 5000;
 
